5a3bae761d9c6f5e193e7e7ff007032e581f0eaa
zqwitt <zach.witt95@gmail.com>
2016-04-27 16:59:07 -0400

    reworked polarity scores
    Polarity scores now integrate multiple occurrences of the terms within
    the database and return an average polarity of the instances

--- sketch.js
+++ sketch.js
@@ -1,13 +1,69 @@
 var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
 var table;
 
+
 function preload() {
   table = loadTable("assets/new.csv", "csv", "header");
 
 }
 
 function setup() {
-  colorMode(HSB,360,100,100);
+  restartFunction();
+
+}
+
+function draw() {
+  // why draw when you can talk?
+}
+
+function showResult() {
+  if (myRec.resultValue === true) {
+
+    var resultSplit = split(myRec.resultString, " ");
+    var result = 0;
+    var results = 0;
+    var colorResult = 60;
+    for (var i = 0; i < resultSplit.length; i++) {
+        var neg = 0;
+        var pos = 0;
+        var negAvg = 0;
+        var posAvg = 0;
+      var rows = table.findRows(resultSplit[i], "T");
+      if (rows !== null && rows.length >0) {
+
+
+        for (var x = 0; x < rows.length; x++) {
+            neg += rows[x].getNum("N");
+            pos += rows[x].getNum("P");
+        }
+        negAvg = neg / rows.length;
+        posAvg = pos / rows.length;
+        
+
+      }
+      result = result + posAvg;
+      result = result - negAvg;
+      results++;
+      console.log(result);
+
+    }
+    var gr = color(140, 60, 90);
+    var re = color(0, 60, 90);
+    var res = map(result / results, -1, 1, 0, 1);
+    colorResult = lerpColor(re, gr, res);
+    console.log("!" + res);
+    background(colorResult);
+    //text(result/results, width / 2, height / 2);
+    text(myRec.resultString, width / 2, height / 2);
+  }
+}
+
+function mouseClicked() {
+  restartFunction();
+}
+
+function restartFunction() {
+  colorMode(HSB, 360, 100, 100);
   // graphics stuff:
   createCanvas(windowWidth, windowHeight);
   background(0, 0, 100);
@@ -19,48 +75,6 @@ function setup() {
   myRec.onResult = showResult;
   myRec.start();
 
-}
-
-function draw() {
-  // why draw when you can talk?
-}
-
-function showResult() {
-  if (myRec.resultValue === true) {
-
-    var resultSplit = split(myRec.resultString," ");
-    var result = 0;
-    var results = 0;
-    var colorResult = 60;
-    for (var i = 0; i < resultSplit.length; i++) {
-
-      var row = table.findRow(resultSplit[i], "T");
-      if (row !== null) {
-        if(row.getNum("P") !== 0){
-          result = result + (row.getNum("P"));
-          results++;
-        }
-        if(row.getNum("N") !== 0){
-          result = result - (row.getNum("N"));
-          results ++;
-        }
-      }
-        if(result === 0){
-          results = 1;
-        }
-        console.log(result);
-      
-    }
-    colorResult = map(result/results,-1,1,0,120);
-    console.log("!"+ colorResult);
-    background(colorResult, 60, 90);
-    //text(result/results, width / 2, height / 2);
-    text(myRec.resultString, width / 2, height / 2);
-  }
-}
-
-function mouseClicked() {
- setup(); 
 }
 
 function windowResized() {
