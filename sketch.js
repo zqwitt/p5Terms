1324f54f18616f45a2df49525c48e09d23a5a0fc
zqwitt <zach.witt95@gmail.com>
2016-04-29 16:58:42 -0400

    !chrome alert. responsive text
    Added a textual alert if the browser is not chrome. Changed font so
    that the window is responsive.

--- sketch.js
+++ sketch.js
@@ -15,6 +15,8 @@ function setup() {
 function draw() {
   // why draw when you can talk?
 }
+var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+
 
 function showResult() {
   if (myRec.resultValue === true) {
@@ -24,21 +26,25 @@ function showResult() {
     var results = 0;
     var colorResult = 60;
     for (var i = 0; i < resultSplit.length; i++) {
-        var neg = 0;
-        var pos = 0;
-        var negAvg = 0;
-        var posAvg = 0;
+      var neg = 0;
+      var pos = 0;
+      var negAvg = 0;
+      var posAvg = 0;
       var rows = table.findRows(resultSplit[i], "T");
-      if (rows !== null && rows.length >0) {
+      if (rows !== null && rows.length > 0) {
 
 
         for (var x = 0; x < rows.length; x++) {
+          if (rows[x].getNum("N") !== 0) {
             neg += rows[x].getNum("N");
+          }
+          if (rows[x].getNum("P") !== 0) {
             pos += rows[x].getNum("P");
+          }
         }
         negAvg = neg / rows.length;
         posAvg = pos / rows.length;
-        
+
 
       }
       result = result + posAvg;
@@ -53,30 +59,37 @@ function showResult() {
     colorResult = lerpColor(re, gr, res);
     console.log("!" + res);
     background(colorResult);
-    //text(result/results, width / 2, height / 2);
+    text(nfc(res * 100, 2) + "%!", width / 2, height / 2 + 40);
     text(myRec.resultString, width / 2, height / 2);
   }
 }
+
 
 function mouseClicked() {
   restartFunction();
 }
 
+var cv;
+
 function restartFunction() {
   colorMode(HSB, 360, 100, 100);
   // graphics stuff:
-  createCanvas(windowWidth, windowHeight);
+  cv = createCanvas(windowWidth, windowHeight);
   background(0, 0, 100);
   fill(0, 0, 0, 255);
   // instructions:
-  textSize(32);
+  textSize(width / 32);
   textAlign(CENTER);
-  text("say something nice or say something mean", width / 2, height / 2);
-  myRec.onResult = showResult;
-  myRec.start();
-
+  if (!is_chrome) {
+    text("sorry! this experiment only works in the chrome browser!", width / 2, height / 2);
+  } else {
+    text("say something nice or say something mean", width / 2, height / 2);
+    myRec.onResult = showResult;
+    myRec.start();
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  restartFunction();
 }
